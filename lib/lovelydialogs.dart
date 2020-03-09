63fd56164259a5818de1dbcf32f95aed1c8a2b69
Vector <victorherberts@outlook.com>
2020-03-09 19:31:17 -0300

    Starting Project

--- lib/lovelydialogs.dart
+++ lib/lovelydialogs.dart
@@ -1,7 +1,41 @@
 library lovelydialogs;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+//lieno tecnologia
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+abstract class _LovelyDialog extends StatelessWidget{
+    
+}
+
+class LovelyDialogInfo extends _LovelyDialog{
+
+  BuildContext context;
+  LovelyDialogInfo(BuildContext this.context);
+
+  show(){
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) => this,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+        Container(
+          height: 200,
+        ),
+        Row(
+          children: <Widget>[
+            
+          ],
+        )
+
+      ],
+    );
+  }
+   
+
 }
